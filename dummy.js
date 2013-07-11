eeb8a3aec62d1df7f574a7ab53fed781891b87a2
Erty Seidel <ertysdl+github@gmail.com>
2013-07-11 16:24:03 -0400

    Bugfix for attack

--- dummy.js
+++ dummy.js
@@ -122,7 +122,7 @@ function findReinforce(game){
 		var origin_country_name = Object.keys(my_countries)[Math.floor(Math.random() * Object.keys(my_countries).length)];
 		for(var border_country_index in board_graph_countries[origin_country_name]["border countries"]){
 			var border_country_name = board_graph_countries[origin_country_name]["border countries"][border_country_index];
-			if(game.countries[border_country_name].owner == my_name){
+			if(game.countries[border_country_name].owner == my_name && game.countries[border_country_name].troops > 1){
 				return {"destination_country": border_country_name, "origin_country": origin_country_name};
 			}
 		}
